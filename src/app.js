4493a526ec98b69256936516b34b0b614deee8d9
wale2002 <gemred2002@gmail.com>
2025-08-29 10:50:18 +0100

    newe

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3000/",
   "http://localhost:8081/",
-  "https://pastwise-solver.vercel.app/",
+  "https://pastwise-solver.vercel.app",
   "http://localhost:5173",
 ];
 
